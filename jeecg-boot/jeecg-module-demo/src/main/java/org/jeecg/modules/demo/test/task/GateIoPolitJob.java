7fb2b3760168bffc286127f89328d7795d51a258
handong106324 <442835857@qq.com>
2025-01-01 18:27:39 +0800

    gate

--- jeecg-boot/jeecg-module-demo/src/main/java/org/jeecg/modules/demo/test/task/GateIoPolitJob.java
+++ jeecg-boot/jeecg-module-demo/src/main/java/org/jeecg/modules/demo/test/task/GateIoPolitJob.java
@@ -9,11 +9,15 @@ import org.jeecg.common.util.SpringContextUtils;
 import org.jeecg.modules.demo.test.entity.GatePilotSymbol;
 import org.jeecg.modules.demo.test.service.IJeecgDynamicDataService;
 import org.jeecg.modules.demo.test.service.IJeecgGatePilotService;
+import org.jeecg.modules.system.alert.AlertType;
+import org.jeecg.modules.system.alert.WeiXinAlert;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 示例不带参定时任务
@@ -30,27 +34,34 @@ public class GateIoPolitJob implements Job {
 		if (null == jeecgService) {
 			jeecgService = SpringContextUtils.getBean(IJeecgGatePilotService.class);
 		}
+
+		List<GatePilotSymbol> list = jeecgService.list();
+		Map<String, GatePilotSymbol> oldMap =
+				list.stream().collect(Collectors.toMap(GatePilotSymbol::getShowPair, v -> v));
 		String s = HttpUtil.get("https://www.gate.io/apiw/v2/pilot/markets?exchange_type=PILOT_SOL_ALL");
 		JSONObject result = JSONObject.parseObject(s);
 		System.out.println(result.toJSONString());
 		JSONArray jsonArray = result.getJSONArray("data");
 		for (int i = 0; i < jsonArray.size(); i++) {
 			JSONObject jsonObject = jsonArray.getJSONObject(i);
-			System.out.println(jsonObject);
 			GatePilotSymbol symbol = jsonObject.toJavaObject(GatePilotSymbol.class);
 
 			symbol.setVol_change(jsonObject.getString("change"));
 
-			QueryWrapper<GatePilotSymbol> queryWrapper = new QueryWrapper<>();
-			List<GatePilotSymbol> showPair = jeecgService.list(queryWrapper.eq("show_pair", symbol.getShowPair()));
-			if (showPair.isEmpty())
+			GatePilotSymbol oldSymbol = oldMap.get(symbol.getShowPair());
+			if (oldSymbol == null)
+			{
 				jeecgService.save(symbol);
+				WeiXinAlert.getInstance().sendMessage(symbol.getShowPair() + "热度变化：" + symbol.is_hot(), AlertType.ETH_ALERT);
+			}
 			else {
-				GatePilotSymbol gatePilotSymbol = showPair.get(0);
-				gatePilotSymbol.setVol_change(symbol.getVol_change());
-				gatePilotSymbol.set_hot(symbol.is_hot());
-				gatePilotSymbol.setLast(symbol.getLast());
-				jeecgService.updateById(gatePilotSymbol);
+				oldSymbol.setVol_change(symbol.getVol_change());
+				if (oldSymbol.is_hot() != symbol.is_hot()) {
+					WeiXinAlert.getInstance().sendMessage(oldSymbol.getShowPair() + "热度变化：" + oldSymbol.is_hot() + "->" + symbol.is_hot(), AlertType.ETH_ALERT);
+					oldSymbol.set_hot(symbol.is_hot());
+				}
+				oldSymbol.setLast(symbol.getLast());
+				jeecgService.updateById(oldSymbol);
 			}
 		}
 	}
