4ffd56bf3e872d49bf5754a9ac99766db60a6318
handong <handong@cjdbi.com>
2025-01-17 13:30:56 +0800

    币种监听 发送微信群

--- jeecg-boot/jeecg-module-demo/src/main/java/org/jeecg/modules/demo/utils/GateApiUtils.java
+++ jeecg-boot/jeecg-module-demo/src/main/java/org/jeecg/modules/demo/utils/GateApiUtils.java
@@ -1,42 +1,79 @@
 package org.jeecg.modules.demo.utils;
 
-import cn.hutool.http.HttpRequest;
-import cn.hutool.http.HttpUtil;
-import com.alibaba.fastjson.JSONObject;
 import io.gate.gateapi.ApiClient;
 import io.gate.gateapi.ApiException;
 import io.gate.gateapi.api.*;
 import io.gate.gateapi.models.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class GateApiUtils {
 
+    private static class SingletonHolder {
+        private static final GateApiUtils INSTANCE = new GateApiUtils();
+    }
+    public static GateApiUtils getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
 
-    public static void main(String[] args) {
-        ApiClient client = new ApiClient();
+    public ApiClient client;
+    public SpotApi spotApi;
+    public List<String> spotSymbols = new ArrayList<>();
+    private Map<String,Double> priceMap = new HashMap<>();
+    private GateApiUtils() {
+        client = new ApiClient();
         client.setApiKeySecret("4717da20946b7d7102a65563fb55d221", "733be1e46138cbe6f8c4ed07f6ff3bf773ce4ab7481671edf8a7b5c17cd7b491");
-        AccountApi accountApi = new AccountApi(client);
-
-        SpotApi spotApi = new SpotApi(client);
-        WalletApi walletApi = new WalletApi(client);
-
-
-//        spotApi.get
-
+        spotApi = new SpotApi(client);
+        List<CurrencyPair> currencies = null;
         try {
+            currencies = spotApi.listCurrencyPairs();
+            for (CurrencyPair currency : currencies) {
+                spotSymbols.add(currency.getBase());
+            }
+        } catch (Exception e) {
 
-            List<CurrencyPair> currencyPairs = spotApi.listCurrencyPairs();
-
-//            for (SpotAccount spotAccount : spotApi.listSpotAccounts().execute()) {
-//                System.out.println(spotAccount.getCurrency() + ":" + spotAccount.getAvailable() + ":" + spotAccount.getLocked());
-//            }
-
-        } catch (ApiException e) {
-            throw new RuntimeException(e);
         }
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        tickerSpot();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        Thread.sleep(10 * 60000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
 
     }
+
+    public Map<String,Double> tickerSpot() {
+
+
+        SpotApi.APIlistTickersRequest apIlistTickersRequest = spotApi.listTickers();
+        List<Ticker> execute = null;
+        try {
+            execute = apIlistTickersRequest.execute();
+            for (Ticker ticker : execute) {
+                System.out.println(ticker.getCurrencyPair());
+                priceMap.put(ticker.getCurrencyPair(), Double.parseDouble(ticker.getLast()));
+            }
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
+
+        return priceMap;
+    }
+
 }
